7d8f17e13564956ca4b4c4043131e8d84376435c
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-10-09 09:14:37 -0300

    Added integration

---
+++ wolox-books/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  form.column.container
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
